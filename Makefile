56b9840fc8ba22b16032f8a080a68b28be3dda75
ivan-tana <ivan-tana@outlook.com>
2022-09-06 15:44:23 +0000

    hsl plugin

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 install:
 	npm install vue@2
 	npm install vue-core-video-player 
+	npm install @core-player/playcore-hls
 run:
 	npm run dev
--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,13 +1,26 @@
 <script>
 import Vue from 'vue'
+
 import VueCoreVideoPlayer from 'vue-core-video-player'
+import HLSCore from '@core-player/playcore-hls'
+
+
 Vue.use(VueCoreVideoPlayer)
+
+export default {
+  data () {
+    return {
+      HLSCore,
+      url:"http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8"
+    }
+  }
+}
 
 </script>
 
 <template>
   <div class="player-container">
-    <vue-core-video-player @play="your_method" src="http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8"></vue-core-video-player>
+    <vue-core-video-player :core="HLSCore" :src="url"></vue-core-video-player>
   </div>
 </template>
 

938cd293426af9b66bee2f6685fda76392030697
ivan-tana <ivan-tana@outlook.com>
2022-09-06 14:10:54 +0000

    add video player

---
+++ Makefile
@@ -0,0 +1,5 @@
+install:
+	npm install
+	npm install vue-core-video-player 
+run:
+	npm run dev
---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,13 @@
+<script>
+import Vue from 'vue'
+import VueCoreVideoPlayer from 'vue-core-video-player'
+Vue.use(VueCoreVideoPlayer)
+
+</script>
+
+<template>
+  <div class="player-container">
+    <vue-core-video-player @play="your_method" src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"></vue-core-video-player>
+  </div>
+</template>
+
